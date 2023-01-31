a04acf226fe151788d86cfb5f7f6ec82950e30d7
CrisRu95 <ecrisru@alumni.uv.es>
2023-01-31 09:55:09 +0100

    Updated design parameters.

--- ExonSurfer/primerDesign/designConfig.py
+++ ExonSurfer/primerDesign/designConfig.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 design_dict = {
-        'PRIMER_PICK_INTERNAL_OLIGO': 0,
         
+        # primer paramters:
         'PRIMER_OPT_SIZE': 20,
         'PRIMER_MIN_SIZE': 17,
         'PRIMER_MAX_SIZE': 35,
@@ -11,23 +11,30 @@ design_dict = {
         'PRIMER_OPT_TM': 59,
         'PRIMER_MIN_TM': 57.5,
         'PRIMER_MAX_TM': 60.5,
-        
-        'PRIMER_PRODUCT_OPT_TM': 80,
-        'PRIMER_PRODUCT_MIN_TM': 65,
-        'PRIMER_PRODUCT_MAX_TM': 90,
+        'PRIMER_TM_FORMULA': 1, # use santaLucia (1998) formula 
         
         'PRIMER_OPT_GC_PERCENT': 50,
         'PRIMER_MIN_GC': 20,
         'PRIMER_MAX_GC': 80,
-        'PRIMER_WT_GC_PERCENT_GT':1,
-        'PRIMER_WT_GC_PERCENT_LT': 1,
         
         'PRIMER_MAX_POLY_X': 5,
-        'PRIMER_INTERNAL_MAX_POLY_X': 5,
-        'PRIMER_MAX_NS_ACCEPTED': 0,
-        
         'PRIMER_GC_CLAMP': 1, 
         'PRIMER_MAX_END_GC': 4,
+
+        # not shown to user
+        'PRIMER_WT_GC_PERCENT_GT':1,
+        'PRIMER_WT_GC_PERCENT_LT': 1,
+                
+        # product parameters
+        'PRIMER_PRODUCT_OPT_TM': 80,
+        'PRIMER_PRODUCT_MIN_TM': 65,
+        'PRIMER_PRODUCT_MAX_TM': 90,
         
-        'PRIMER_PRODUCT_SIZE_RANGE': [[80, 170]]
+        'PRIMER_PRODUCT_SIZE_RANGE': [[80, 170]],
+        
+        # PCR parametres
+        'PRIMER_SALT_DIVALENT': 1.5,
+        'PRIMER_SALT_MONOVALENT': 50,
+        'PRIMER_SALT_CORRECTIONS': 1, # use santaLucia (1998) formula 
+        'PRIMER_DNTP_CONC': 0.6, 
         } 
