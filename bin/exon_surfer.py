7919cfdf90597d7196a34c602238dc8680480971
pamonlan <55535044+pamonlan@users.noreply.github.com>
2023-02-01 09:41:53 +0100

    Add species as argument

--- bin/exon_surfer.py
+++ bin/exon_surfer.py
@@ -13,6 +13,8 @@ def arguments_parser():
     This function parses arguments
     """
     parser = argparse.ArgumentParser(description = "Design primers in the specified dir")
+    parser.add_argument("--species","-sp",dest="species",
+                       help = "Species of the gene", default = "homo_sapiens")
     parser.add_argument("--gene", "-g", dest = "gene", 
                         help = "Name of the gene to target (ENSEMBL ID)")
     parser.add_argument("--transcript", "-t", dest = "transcript",default="ALL", 
@@ -29,8 +31,8 @@ def main():
     args = arguments_parser()
     
     # Construct transcripts dictionary
-    gene, transcripts, path_out = args.gene, args.transcript, args.path_out
-    CreatePrimers(gene, transcripts, path_out)
+    gene, transcripts, path_out, species = args.gene, args.transcript, args.path_out, args.species
+    CreatePrimers(gene = gene, transcripts = transcripts, species = species, path_out = path_out)
 
 if __name__ == "__main__":
     main()
