6fb07f9a77f1673cb014cd93929ef482c7d27012
pamonlan <55535044+pamonlan@users.noreply.github.com>
2023-03-30 09:05:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
    name='ExonSurfer',
-   version='0.1.55',
+   version='0.1.6',
    author='Pablo Monfort-Lanzas, Cristina Rusu',
    author_email='pablo.monfort@i-med.ac.at',
    packages=['ExonSurfer','ExonSurfer.specificity','ExonSurfer.ensembl',\
